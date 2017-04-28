ecf53aa7ea058a1a2e28332f658d2bcdb9d9ae28
szucscintialia <szucs.cintialia@gmail.com>
2017-04-28 04:47:40 -0700

    Tests

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -14,6 +14,11 @@ public class GT4500 implements SpaceShip {
     this.primaryTorpedoStore = new TorpedoStore(10);
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
+  
+  public GT4500(TorpedoStore primaryTS, TorpedoStore secondaryTS) {
+	    this.primaryTorpedoStore = primaryTS;
+	    this.secondaryTorpedoStore = secondaryTS;
+	  }
 
   public boolean fireLasers(FiringMode firingMode) {
     // TODO not implemented yet
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,31 +9,47 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockPrimaryStore;
+  private TorpedoStore mockSecondaryStore;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+	mockPrimaryStore = mock(TorpedoStore.class);
+	mockSecondaryStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimaryStore, mockSecondaryStore);
   }
 
   @Test
   public void fireTorpedos_Single_Success(){
     // Arrange
-
-    // Act
+	when(mockPrimaryStore.fire(1)).thenReturn(true);
+	when(mockSecondaryStore.fire(1)).thenReturn(true);
+    
+	  
+	// Act
     boolean result = ship.fireTorpedos(FiringMode.SINGLE);
-
+	
     // Assert
+    verify(mockPrimaryStore, times(1)).fire(1);
+    verify(mockSecondaryStore, times(0)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedos_All_Success(){
     // Arrange
-
+	when(mockPrimaryStore.isEmpty()).thenReturn(false);
+	when(mockSecondaryStore.isEmpty()).thenReturn(false);
+	when(mockPrimaryStore.fire(1)).thenReturn(true);
+	when(mockSecondaryStore.fire(1)).thenReturn(true);
+	
+		
     // Act
     boolean result = ship.fireTorpedos(FiringMode.ALL);
 
     // Assert
+    verify(mockPrimaryStore, times(1)).fire(1);
+    verify(mockSecondaryStore, times(1)).fire(1);
     assertEquals(true, result);
   }
 
